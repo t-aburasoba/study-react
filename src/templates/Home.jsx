c13ac68e30f45bf4c0c2f2393e4f4c474a53cf87
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2021-03-13 10:00:27 +0900

    add operations

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { getUserId } from '../reducks/users/selectors'
+import { getUserId, getUserName } from '../reducks/users/selectors'
 
 const Home = () => {
     const selector = useSelector(state => state);
     const uid = getUserId(selector)
+    const username = getUserName(selector)
 
     return(
         <div>
             <h2>Home</h2>
-            <p>{uid}</p>
+            <p>ユーザーID{uid}</p>
+            <p>ユーザー名{username}</p>
         </div>
     )
 }
